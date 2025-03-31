dea091d68a2026494146a68d61041d30adaee773
Xontes <165090317+Xontes@users.noreply.github.com>
2025-03-31 23:20:22 +0400

    Add files via upload

---
+++ Weapon/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Weapon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon primaryWeapon1 = new Weapon("Bulldog", 24);
+            //primaryWeapon1.GetInfo();
+            //primaryWeapon1.Reload();
+            //primaryWeapon1.GetInfo();
+            //primaryWeapon1.Shoot();
+            //primaryWeapon1.Shoot();
+            //primaryWeapon1.Shoot();
+            //primaryWeapon1.GetInfo();
+            //primaryWeapon1.ChangeFireMode();
+            //primaryWeapon1.Shoot();
+            //primaryWeapon1.GetInfo();
+            //primaryWeapon1.Reload();
+            //primaryWeapon1.GetInfo();
+            while (true)
+            {
+                Console.WriteLine("Enter a command:");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "/0":
+                        primaryWeapon1.GetInfo();
+                        break;
+
+                    case "/1":
+                        primaryWeapon1.Shoot();
+                        break;
+
+                    case "/2":
+                        primaryWeapon1.GetRemainBulletCount();
+                        break;
+
+                    case "/3":
+                        primaryWeapon1.Reload();
+                        break;
+
+                    case "/4":
+                        primaryWeapon1.ChangeFireMode();
+                        break;
+
+                    case "/5":
+                        Console.WriteLine("Program stopped.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid command.Try again.");
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Weapon/Weapon.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Weapon deyə class yaradılır.
+//Darağın güllə tutumu, Daraqdakı güllə sayı, atış modu (təkli və ya avtomatik) propertileri verilir.
+//Shoot() -  metodu hər çağırıldıqda fire moduna gore ya 1 güllə atır yada avtomatik gulleni bosaldir.(Ekranda bunu bildirir)
+//GetRemainBulletCount() - darağın tam dolması üçün lazım olan güllə sayını qaytarır.
+//Reload() - darağı yenidən doldurur.
+//ChangeFireMode() - Atış modunu dəyişir.
+//Bütün məlumatları doldurmadan obyekt yaratmaq olmasın.
+//0 - İnformasiya almaq üçün
+//1 - Shoot metodu üçün (hazirki moda uygun gulle atacaq)
+//2 - GetRemainBulletCount metodu üçün
+//3 - Reload metodu üçün
+//4 - ChangeFireMode metodu üçün
+//5 - Proqramdan dayandırmaq üçün
+//qısayoldur.
+namespace Weapon
+{
+    public class FireMode
+    {
+        public static readonly string singleShot = "singleshot mode";
+        public static readonly string multiShot = "multisshot mode";
+    }
+
+    public class Weapon : FireMode
+    {
+        public string name { get; set; }
+        public int bulletCapacity { get; set; }
+        public int currentBullets { get; set; }
+        public string currentFireMode = singleShot;
+
+        public Weapon(string name,int bulletCapacity)
+        {
+            this.name = name;
+            this.bulletCapacity = bulletCapacity;
+          
+        }
+
+        public void GetInfo()
+        {
+            
+            Console.WriteLine(name);
+            Console.WriteLine($"Weapons Bullet Capacity:{bulletCapacity}");
+            Console.WriteLine($"Weapon Fire Mode:{currentFireMode}");
+            Console.WriteLine($"Remaining bullets:{currentBullets}");
+        }
+
+        public void Shoot()
+        {
+            if (currentBullets > 0)
+            {
+                if (currentFireMode == singleShot)
+                {
+                    currentBullets--;
+                    Console.WriteLine("A bullet has been shot.");
+                }
+                else
+                {
+                    currentBullets = 0;
+                    Console.WriteLine("All bullets have been shot.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are no bullets in the shell.Please reload first.");
+            }
+        }
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine($"Need {bulletCapacity - currentBullets} more bullets to fill the shell.");
+        }
+        public void Reload()
+        {
+            currentBullets = bulletCapacity;
+            Console.WriteLine("Weapon reloaded ready to fire.");
+        }
+        public void ChangeFireMode()
+        {
+            if (currentFireMode == singleShot)
+            {
+                currentFireMode = multiShot;
+                Console.WriteLine("Firemode:multishot");
+            }
+            else
+            {
+                currentFireMode = singleShot;
+                Console.WriteLine("Firemode:singleshot");
+            }
+        }
+    }
+}  
