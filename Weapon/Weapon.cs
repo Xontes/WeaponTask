d8337f9f0a217d3698bd23bc46efcc2a1a33f50b
Xontes <165090317+Xontes@users.noreply.github.com>
2025-03-31 11:38:09 -0800

    Weapon.cs

--- Weapon/Weapon.cs
+++ Weapon/Weapon.cs
@@ -3,20 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Weapon deyə class yaradılır.
-//Darağın güllə tutumu, Daraqdakı güllə sayı, atış modu (təkli və ya avtomatik) propertileri verilir.
-//Shoot() -  metodu hər çağırıldıqda fire moduna gore ya 1 güllə atır yada avtomatik gulleni bosaldir.(Ekranda bunu bildirir)
-//GetRemainBulletCount() - darağın tam dolması üçün lazım olan güllə sayını qaytarır.
-//Reload() - darağı yenidən doldurur.
-//ChangeFireMode() - Atış modunu dəyişir.
-//Bütün məlumatları doldurmadan obyekt yaratmaq olmasın.
-//0 - İnformasiya almaq üçün
-//1 - Shoot metodu üçün (hazirki moda uygun gulle atacaq)
-//2 - GetRemainBulletCount metodu üçün
-//3 - Reload metodu üçün
-//4 - ChangeFireMode metodu üçün
-//5 - Proqramdan dayandırmaq üçün
-//qısayoldur.
 namespace Weapon
 {
     public class FireMode
